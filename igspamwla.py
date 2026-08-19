1d9c15c14d4ad43a87fc26fe04c44435b879010d
rudru-maker <nabokinacu8@gmail.com>
2026-08-19 10:25:30 +0530

    Update OWNER_TG_ID and BOT_TOKEN environment variables

--- igspamwla.py
+++ igspamwla.py
@@ -38,8 +38,8 @@ logging.basicConfig(
 
 AUTHORIZED_FILE = 'authorized_users.json'
 TASKS_FILE = 'tasks.json'
-OWNER_TG_ID = int(os.getenv("6677832525", "0"))
-BOT_TOKEN = os.getenv("8828882943:AAEjOfQZ2A3YZ1lDkD8r0zCxCUxmgHfjn-4", "")
+OWNER_TG_ID = int(os.getenv("667725", "0"))
+BOT_TOKEN = os.getenv("8828882943:AAEjO2A3YZ1lDkD8r0zCxCUxmgHfjn-4", "")
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
 
 authorized_users = []  # list of {'id': int, 'username': str}
